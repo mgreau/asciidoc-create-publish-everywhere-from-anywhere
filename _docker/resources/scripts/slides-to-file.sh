fb1b8e62f34ed5dc0e4ad687f2dec1502ffb891c
Maxime Gréau <greaumaxime@gmail.com>
2015-06-17 02:24:26 +0200

    Comments Dockerfile + webpage

--- _docker/resources/scripts/slides-to-file.sh
+++ _docker/resources/scripts/slides-to-file.sh
@@ -3,6 +3,6 @@
 for (( ; ; ))
 do
   rsync -avz /documents/assets /output/
-  asciidoctor --trace -o /output/slides.html -T /asciidoctor-backends/slim/dzslides /documents/slides.adoc
+  asciidoctor --trace -o /output/slides.html -a link-assets -T /asciidoctor-backends/slim/dzslides /documents/slides.adoc
   sleep 2
 done
